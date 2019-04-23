90bcb48e244f340660f3199e28d14adabdb4039f
EugeneTimofeev <makoron@mail.ru>
2019-04-23 21:38:57 +0300

    First task

---
+++ convert.py
@@ -0,0 +1,49 @@
+import json
+
+
+class JsonToHtml:
+
+    def __init__(self):
+        self.__json_data = ''
+        self.__html_data = ''
+
+    @property
+    def json_data(self):
+        return self.__json_data
+
+    @json_data.setter
+    def json_data(self, value):
+        self.__json_data = value
+
+    @property
+    def html_data(self):
+        return self.__html_data
+
+    @html_data.setter
+    def html_data(self, value):
+        self.__html_data = value
+
+    def load_json(self, file_name):
+        with open(file_name, "r") as f:
+            self.json_data = json.load(f)
+
+    def json_to_html(self):
+        for data in self.json_data:
+            for key in data:
+                if key == 'title':
+                    self.html_data += '<h1>' + data[key] + '</h1>'
+                if key == 'body':
+                    self.html_data += '<p>' + data[key] + '</p>'
+
+    def save_html(self, file_name):
+        with open(file_name, "w") as f:
+            f.write(self.html_data)
+
+
+if __name__ == '__main__':
+    input_file = "source.json"
+    output_file = "index.html"
+    mydata = JsonToHtml()
+    mydata.load_json(input_file)
+    mydata.json_to_html()
+    mydata.save_html(output_file)
